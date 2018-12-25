dfb0cf469eee9a2847153ee5c457b43144fbe797
Dmitriy Dulkin <37985913+AmazingSuka@users.noreply.github.com>
2018-12-25 22:51:38 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,24 +8,7 @@ import App from './App';
 
 // Init VK App
 connect.send('VKWebAppInit', {});
-const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('cotopesDb', 'root', 'qwertyAhuel', {
-    host: 'cotopesinstance.cmdc44d5brv4.us-east-2.rds.amazonaws.com',
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: true
-    }
-});
-
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
 
 
 // Если вы хотите, чтобы ваше веб-приложение работало в оффлайне и загружалось быстрее,
